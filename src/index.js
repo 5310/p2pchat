369d99ca5e7ce00b9c85912a5cb26f784573e5a7
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2018-01-29 18:35:05 +0530

    SW enabled... ...even though I have no idea how to get SW to point to Parcel bundles or Parcel to bundle stuff pointed to buy the SW

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ document.onreadystatechange = async function () {
   }
 }
 
-// // Setup service worker:
-// if (location.protocol === 'http:') location.protocol = 'https:'
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('sw.js')
-//     .then(reg => console.log('Service Worker registered', reg))
-//     .catch(err => console.error('Service Worker **not** registered', err))
-// } else {
-//   console.warn('Service Worker not supported in this browser')
-// }
+// Redirect to secure
+// if (location.protocol === 'http:') location.protocol = 'https:' //DEBUG: Don't use with Parcel's HMR!
+
+// Setup service worker:
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js')
+    .then(reg => console.log('Service Worker registered', reg))
+    .catch(err => console.warn('Service Worker **not** registered', err))
+} else {
+  console.warn('Service Worker not supported in this browser')
+}
