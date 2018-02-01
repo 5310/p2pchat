fc46fa51cf00d950a2725b0538392173d1d38792
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2018-02-01 20:26:39 +0530

    Scroll posts automatically.

--- src/app/app.js
+++ src/app/app.js
@@ -162,6 +162,7 @@ export default class App {
         const $templateChatContent = document.querySelector('template#app-chat__content')
         $newestChat.querySelector('.app-chat__contents').appendChild(template($templateChatContent, post))
       }
+      $chats.querySelector('.app-chat:last-child').scrollIntoView()
     }
   }
 }
