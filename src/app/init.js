a69ea681edbfaa81b03536e05f51f10b278efa1e
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2018-02-01 20:04:04 +0530

    Delinted

--- src/app/init.js
+++ src/app/init.js
@@ -1,13 +1,11 @@
 import App from './app'
-import Post from './post'
-import Channel from './channel'
 
 export default function init (node) {
   const app = new App(node)
   app.joinChannel({ name: 'global', key: '' })
   app.joinChannel({ name: 'test', key: 'test' })
   setInterval(
-    () => app.sendPost(new Post({ content: Date.now() })),
+    () => app.sendPost({ content: Date.now() }),
     5000
   ) // DEBUG: Mock
   return app
