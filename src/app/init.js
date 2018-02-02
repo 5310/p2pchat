9a2954e6799ccf2a788e2cc2b7ab5a040db81168
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2018-02-02 17:15:20 +0530

    Fixed scrolling.

--- src/app/init.js
+++ src/app/init.js
@@ -2,11 +2,5 @@ import App from './app'
 
 export default function init (node) {
   const app = new App(node)
-  // app.joinChannel({ name: 'global', key: '' })
-  // app.joinChannel({ name: 'test', key: 'test' })
-  setInterval(
-    () => app.sendPost({ content: Date.now() }),
-    2000
-  ) // DEBUG: Mock
   return app
 }
