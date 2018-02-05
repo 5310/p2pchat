06751ef438787680c80f6a8096e93f1d1dc7b42a
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2018-02-05 12:10:14 +0530

    Simplified runtime setup.

--- src/libp2p/node.js
+++ src/libp2p/node.js
@@ -1,23 +1,21 @@
 import WebRTCStar from 'libp2p-webrtc-star'
 import WebSockets from 'libp2p-websockets'
 import Multiplex from 'libp2p-multiplex'
-import SECIO from 'libp2p-secio'
 import libp2p from 'libp2p'
 
 export default class Node extends libp2p {
   constructor (peerInfo) {
-    const wstar = new WebRTCStar()
+    const webrtcstar = new WebRTCStar()
     super({
       transport: [
-        wstar,
+        webrtcstar,
         new WebSockets(),
       ],
       connection: {
         muxer: [Multiplex],
-        crypto: [SECIO], // NOTE: Can be removed to simplify the project :]
       },
       discovery: [
-        wstar.discovery,
+        webrtcstar.discovery,
       ]
     }, peerInfo)
   }
