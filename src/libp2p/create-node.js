903fa7b52da2aae671a37be76459403a02312bb6
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2018-02-05 14:10:39 +0530

    Errors when creating peers.

--- src/libp2p/create-node.js
+++ src/libp2p/create-node.js
@@ -7,16 +7,17 @@ const createPeerInfo = pify(PeerInfo.create)
 export default async function createNode () {
   const peerId = window.localStorage.getItem('peerId')
   const peerInfo = peerId ? await createPeerInfo(JSON.parse(peerId)) : await createPeerInfo()
+  if (!peerInfo) throw new Error('PerrInfo could not be generated.')
   window.localStorage.setItem('peerId', JSON.stringify(peerInfo.id.toJSON()))
 
   const peerIdStr = peerInfo.id.toB58String()
-  const ma = `/dns4/star-signal.cloud.ipfs.team/tcp/443/wss/p2p-webrtc-star/ipfs/${peerIdStr}`
-  peerInfo.multiaddrs.add(ma)
+  peerInfo.multiaddrs.add(`/dns4/star-signal.cloud.ipfs.team/tcp/443/wss/p2p-webrtc-star/ipfs/${peerIdStr}`)
 
   const node = new Node(peerInfo)
+  if (!node) throw new Error('LibP2P node could not be initialized.')
   node.pubsub = new FloodSub(node)
   node.pubsub.start((err) => {
-    if (err) return console.error('PubSub failed to start.')
+    if (err) throw new Error('PubSub failed to start.')
   })
 
   return node
